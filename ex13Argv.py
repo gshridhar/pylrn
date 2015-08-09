87fff9b0402003ab8802f8a782ff1713ef31c1bb
gshridhar <shridhargadekar@gmail.com>
2015-08-10 02:43:58 +0530

    getting argument with argc

---
+++ ex13Argv.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# Author: shridhar gadekar
+# Date: 10th August 2015
+# Subject: Exercises for getting input at command line with argv
+
+from sys import argv
+
+script, first, second, third = argv
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
+
