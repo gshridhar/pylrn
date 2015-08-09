23f21867865f8202ca8b85a29d678714042f2808
gshridhar <shridhargadekar@gmail.com>
2015-08-09 21:39:35 +0530

    escape characters and escape sequences

---
+++ escapeSqnc.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# Author: shridhar gadekar
+# Date: 9th August 2015
+# Subject: Escape single quote and double quotes
+
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishes
+\t* Catnip\n\t* Grass
+"""
+
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
