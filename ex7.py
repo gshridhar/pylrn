fceac704426f8af6f49d4d2780afe714370ad0ba
gshridhar <shridhargadekar@gmail.com>
2015-08-09 21:12:07 +0530

    exercise on string manipulation

---
+++ ex7.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# Author: shridhar gadekar
+# Date: 9th August 2015
+# Subject: Exercises for string
+
+# will print lines
+print "Mary had a little lamb."
+# will print with snow as string
+print "Its fleece was while as %s." % 'snow'
+# will print . ten times
+print "." * 10 
+
+# Assign string to variable
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# print all variables all together in one line. ',' will keep two print statements in one line
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end9 + end10 + end11 + end12
