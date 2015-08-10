8089c3b9776d074ed5029e97d7cebdde0fca1b62
gshridhar <shridhargadekar@gmail.com>
2015-08-11 05:14:31 +0530

    more functions with FILE IO

---
+++ functionsFilesEx20.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+# Author: shridhar gadekar
+# Date: 11th August 2015
+# Subject: Exercises for Functions with files
+
+from sys import argv
+
+script, input_file = argv
+
+def print_all(f):
+	print f.read()
+
+def rewind(f):
+	f.seek(0)
+
+def print_a_line(line_count, f):
+	print line_count, f.readline()
+
+current_file = open(input_file)
+print "First let's print the whole file:\n"
+
+print_all(current_file)
+
+print "Now let's rewind, kind of like a tape."
+
+rewind(current_file)
+
+print "Let's print three lines:"
+
+current_line = 1
+print_a_line(current_line, current_file)
+
+current_line = current_line + 1
+print_a_line(current_line, current_file)
+
+current_line = current_line + 1
+print_a_line(current_line, current_file)
+
+current_file.close()
