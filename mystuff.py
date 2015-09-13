c842a0b493174c3fc86726a430d6c303a7477e85
gshridhar <shridhargadekar@gmail.com>
2015-09-13 17:26:28 +0530

    module

---
+++ mystuff.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# Author: shridhar gadekar
+# Date: 13th September 2015
+# Subject: Example on how modules work
+
+# This file will have some functions and variable which then could be used by other program by importing this file
+
+# This is just a function
+def apple():
+   print "I am apple!"
+
+# This is just a variable
+tangerine = "Living reflections of dream!"
+
