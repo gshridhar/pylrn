79c77f47739cb4347696a57c35898bc7b5b9b20e
gshridhar <shridhargadekar@gmail.com>
2016-09-28 16:31:39 +0530

    t

---
+++ process_start.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+#from os import call
+import os
+import subprocess
+
+class myClass:
+   i = 1234
+   def f(self):
+      
+      return "Hello World"
+
+
+x = myClass()
+print x.i
+print x.f()
